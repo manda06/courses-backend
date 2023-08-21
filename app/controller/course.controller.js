d158489c67ee5ff22738a51b0d0225dad6624df1
manda06 <amanda.bass@eagles.oc.edu>
2023-08-20 20:36:54 -0500

    created files for backend

---
+++ app/controller/course.controller.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+const Course = db.courses;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Course
+exports.create = (req, res) => {
+  
+};
+
+// Retrieve all Courses from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single Course with an id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Course by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Course with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Courses from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
+// Find all published Courses
+exports.findAllPublished = (req, res) => {
+  
+};
