3e1a5cb4744f490f492e6b228494487c65a849c5
manda06 <amanda.bass@eagles.oc.edu>
2023-09-01 10:07:23 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
+require("dotenv").config();
+
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
+const db = require("./app/models");
+
+db.sequelize.sync();
+
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors());
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//const app = express();
-//app.use(...);
-
-const db = require("./app/models");
-db.sequelize.sync();
+app.use(express.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+
 require("./app/routes/course.routes")(app);
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3015;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+}
+
+module.exports = app;
