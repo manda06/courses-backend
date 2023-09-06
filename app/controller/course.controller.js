56158d83b9e9b068fde22c8bcd292d8f4bf1ecc3
manda06 <amanda.bass@eagles.oc.edu>
2023-09-06 10:21:09 -0500

    updated course.controller changed Tutorial->Couses

--- app/controller/course.controller.js
+++ app/controller/course.controller.js
@@ -1,7 +1,7 @@
 const db = require("../models");
-const Tutorial = db.tutorial;
+const Course = db.course;
 const Op = db.Sequelize.Op;
-// Create and Save a new Tutorial
+// Create and Save a new Course
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.title) {
@@ -10,51 +10,51 @@ exports.create = (req, res) => {
     });
     return;
   }
-  // Create a Tutorial
-  const tutorial = {
+  // Create a Course
+  const course = {
     title: req.body.title,
     description: req.body.description,
     published: req.body.published ? req.body.published : false,
     userId: req.body.userId,
   };
-  // Save Tutorial in the database
-  Tutorial.create(tutorial)
+  // Save Course in the database
+  course.create(course)
     .then((data) => {
       res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Tutorial.",
+          err.message || "Some error occurred while creating the Course.",
       });
     });
 };
-// Retrieve all Tutorials from the database.
+// Retrieve all Courses from the database.
 exports.findAll = (req, res) => {
   const title = req.query.title;
   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-  Tutorial.findAll({ where: condition })
+  course.findAll({ where: condition })
     .then((data) => {
       res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving tutorials.",
+          err.message || "Some error occurred while retrieving coures.",
       });
     });
 };
 
-// Find a single Tutorial with an id
+// Find a single Course with an id
 exports.findAllForUser = (req, res) => {
   const userId = req.params.userId;
-  Tutorial.findAll({ where: { userId: userId } })
+  course.findAll({ where: { userId: userId } })
     .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: `Cannot find Tutorials for user with id=${userId}.`,
+          message: `Cannot find courses for user with id=${userId}.`,
         });
       }
     })
@@ -62,88 +62,88 @@ exports.findAllForUser = (req, res) => {
       res.status(500).send({
         message:
           err.message ||
-          "Error retrieving Tutorials for user with id=" + userId,
+          "Error retrieving Courses for user with id=" + userId,
       });
     });
 };
-// Find a single Tutorial with an id
+// Find a single Course with an id
 exports.findOne = (req, res) => {
   const id = req.params.id;
-  Tutorial.findByPk(id)
+  course.findByPk(id)
     .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: `Cannot find Tutorial with id=${id}.`,
+          message: `Cannot find Course with id=${id}.`,
         });
       }
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Error retrieving Tutorial with id=" + id,
+        message: err.message || "Error retrieving Course with id=" + id,
       });
     });
 };
-// Update a Tutorial by the id in the request
+// Update a Course by the id in the request
 exports.update = (req, res) => {
   const id = req.params.id;
-  Tutorial.update(req.body, {
+  course.update(req.body, {
     where: { id: id },
   })
     .then((num) => {
       if (num == 1) {
         res.send({
-          message: "Tutorial was updated successfully.",
+          message: "Course was updated successfully.",
         });
       } else {
         res.send({
-          message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`,
+          message: `Cannot update Course with id=${id}. Maybe Course was not found or req.body is empty!`,
         });
       }
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Error updating Tutorial with id=" + id,
+        message: err.message || "Error updating Course with id=" + id,
       });
     });
 };
-// Delete a Tutorial with the specified id in the request
+// Delete a Course with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
-  Tutorial.destroy({
+  course.destroy({
     where: { id: id },
   })
     .then((num) => {
       if (num == 1) {
         res.send({
-          message: "Tutorial was deleted successfully!",
+          message: "COurse was deleted successfully!",
         });
       } else {
         res.send({
-          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`,
+          message: `Cannot delete Course with id=${id}. Maybe Course was not found!`,
         });
       }
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || "Could not delete Tutorial with id=" + id,
+        message: err.message || "Could not delete Course with id=" + id,
       });
     });
 };
-// Delete all Tutorials from the database.
+// Delete all Courses from the database.
 exports.deleteAll = (req, res) => {
-  Tutorial.destroy({
+  course.destroy({
     where: {},
     truncate: false,
   })
     .then((nums) => {
-      res.send({ message: `${nums} Tutorials were deleted successfully!` });
+      res.send({ message: `${nums} Courses were deleted successfully!` });
     })
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all tutorials.",
+          err.message || "Some error occurred while removing all courses.",
       });
     });
 };
